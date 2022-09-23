256ae13115949cefe952ada46896422b463ce32a
renaud <renaud.mariana@tech.kuantic.com>
2022-09-23 18:25:08 +0200

    update figures bench

--- src/hwclock_bench.erl
+++ src/hwclock_bench.erl
@@ -1,44 +1,46 @@
 -module(hwclock_bench).
 
-
-
 -ignore_xref([start/0]).
 -export([ start/0
 	]).
 
 %%--------------------------------------------------------
 %% erl -noshell -pa _build/default/lib/hwclock/ebin -s hwclock_bench | tee output_eee.dat
--define(REFRESH_SCREEN, 100*?E_TIMER).
--define(E_TIMER, 1000).
+-define(REFRESH_SCREEN, timer:seconds(10)).
+-define(GAME_OVER, 60*?REFRESH_SCREEN).
+-define(E_TIMER, 100). %% ms.
 
 -record(state, {e_clock_cumul       ::integer(), 
 		hw_clock_cumul      ::integer(),
 		timer         ::port(), 
-		bpm           ::integer(),
-		offset      ::integer()
+		bpm           ::byte(),
+		start_us         ::term(),
+		hwclock_every_ms :: integer(),
+		iter      ::integer()
 	       }).
 
 
 -spec start() -> true.
 start()  ->
-    BPM = 60, NTicks= 128,   %% (BPM/60)*(NTicks/128) => 1000 ms 
-    Tick_interval_ms = 1000 * BPM * NTicks div 60 div 128,
-    io:format("~nhwclock:open(_BPM=~p, _NTicks=~p)  => tick interval = ~p ms.~n~n",
-	      [BPM, NTicks, Tick_interval_ms]),
+    BPM = 60,   %% between 1..255, 60 means 60 beats per minute
+    NTicks= 128, %% between 1..255, nticks = 128 with bpm = 60 ensures we receive an hwclock event every 1 second
+    HWCLOCK_interval_ms = 1000 * 60 * NTicks div BPM div 128,
+
+    io:format("~nhwclock:open(_BPM=~p, _NTicks=~p)~nevents scheduled every - erlang:~p ms, hwclock:~p ms~nstats are reported every ~p ms.~n",
+	      [BPM, NTicks, ?E_TIMER, HWCLOCK_interval_ms, ?REFRESH_SCREEN]),
     Port = hwclock:open(BPM, NTicks),
-    Now = now_s(erlang:timestamp()),
+    Now = os:timestamp(),
     erlang:start_timer(?E_TIMER, self(), erlang_timer),
     erlang:start_timer(?REFRESH_SCREEN, self(), stat_collect),
-    erlang:start_timer(60*?REFRESH_SCREEN, self(), game_over),
-    loop_bench(#state{offset=Now, timer=Port, bpm=BPM, e_clock_cumul=0, hw_clock_cumul=0}).
-
+    erlang:start_timer(?GAME_OVER, self(), game_over),
+    loop_bench(#state{start_us=Now, timer=Port, bpm=BPM, e_clock_cumul=0, hw_clock_cumul=0, hwclock_every_ms= HWCLOCK_interval_ms}).
 
 loop_bench(S)  ->
     receive
 	{_Port, {data, _Opaque}} -> 
-	    Tick = hwclock:get_tick(_Opaque),
-	    ElapsedMs = hwclock:get_tick_ms(S#state.bpm, _Opaque),
-	    io:format("Elapsed, ticks: ~p, ticks_ms: ~p ms.~n", [Tick, ElapsedMs]),
+	    %% Tick = hwclock:get_tick(Opaque),
+	    %% ElapsedMs = hwclock:get_tick_ms(S#state.bpm, Opaque),
+	    %% io:format("hw new data, tick number: ~p, ticks_ms: ~p ms.~n", [Tick, ElapsedMs]),
 	    Cumul = S#state.hw_clock_cumul,
 	    loop_bench(S#state{hw_clock_cumul=Cumul+1});
 
@@ -49,23 +51,17 @@ loop_bench(S)  ->
 
 	{timeout, _, stat_collect} ->
 	    erlang:start_timer(?REFRESH_SCREEN, self(), stat_collect),
-	    Elapsed = now_s(erlang:timestamp()) - S#state.offset,
-	    io:format("~p\t~p\t~p~n",[Elapsed, S#state.e_clock_cumul- Elapsed, S#state.hw_clock_cumul-Elapsed]),
+	    Elapsed_us =  timer:now_diff(os:timestamp(),S#state.start_us),
+	    io:format("stats after ~p us.\taccumulated events erlang:~p, hwclock:~p\tdrift us. erl versus hwclock: ~p~n",[Elapsed_us,
+														    S#state.e_clock_cumul, S#state.hw_clock_cumul,
+														    S#state.e_clock_cumul*?E_TIMER - S#state.hw_clock_cumul*S#state.hwclock_every_ms]),
 	    loop_bench(S);
 
 	{timeout, _, game_over} ->
 	    io:format("elapsed ! ~n"),
 	    hwclock:stop(S#state.timer);
 
-	closed ->
-	    io:format("The WebSocket was CLOSED!~n"),
+	Tellme ->
+	    io:format("abort on event ~w  ~n", [Tellme]),
 	    hwclock:stop(S#state.timer);
-
-	_Ignore ->
-	    io:format("_Ignore ~w  ~n", [_Ignore]),
-	    loop_bench(S)
     end.
-
-now_s({_MegaSecs,Secs,_MicroSecs}) ->
-    Secs.
-
