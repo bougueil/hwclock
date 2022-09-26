a01b1f95cb6a272a40ab6375e5ab8a709b068134
renaud <rmariana@gmail.com>
2022-09-26 13:42:10 +0200

    rebar3 fmt

--- src/hwclock.erl
+++ src/hwclock.erl
@@ -14,13 +14,12 @@
 
 -module(hwclock).
 
-
--export([ open/2, 
-	  get_tick/1,  
-	  get_tick_ms/2, 
-	  stop/1
-	]).
-
+-export([
+    open/2,
+    get_tick/1,
+    get_tick_ms/2,
+    stop/1
+]).
 
 %% To help with testing internal functions
 -ifdef(TEST).
@@ -28,55 +27,56 @@
 -compile([export_all]).
 -endif.
 
-
--define(HWRES, 15625).    %% 15625 us. => RESOLUTION 2
--define(HWCLOCK_START, 3).  
+%% 15625 us. => RESOLUTION 2
+-define(HWRES, 15625).
+-define(HWCLOCK_START, 3).
 -define(HWCLOCK_STOP, 4).
 
-
--spec open(byte(),byte()) -> port().
+-spec open(byte(), byte()) -> port().
 open(BPM, NTicks) when
-      NTicks >= 0,
-      NTicks < 256,
-      BPM >= 0,
-      BPM < 256->
+    NTicks >= 0,
+    NTicks < 256,
+    BPM >= 0,
+    BPM < 256
+->
     Fdlink = "fdlink_" ++ ?MODULE_STRING,
-    Exec = case code:priv_dir(?MODULE) of
-	       {error, bad_name} ->
-		   case code:which(?MODULE) of
-		       Filename when is_list(Filename) ->
-			   filename:join([filename:dirname(Filename),"../priv", Fdlink]);
-		       _ ->
-			   filename:join("../priv", Fdlink)
-		   end;
-	       Dir ->
-		   filename:join(Dir, Fdlink)
-	   end,
-    Port = open_port( {spawn, Exec}, [{packet, 2}, binary, exit_status]),
+    Exec =
+        case code:priv_dir(?MODULE) of
+            {error, bad_name} ->
+                case code:which(?MODULE) of
+                    Filename when is_list(Filename) ->
+                        filename:join([filename:dirname(Filename), "../priv", Fdlink]);
+                    _ ->
+                        filename:join("../priv", Fdlink)
+                end;
+            Dir ->
+                filename:join(Dir, Fdlink)
+        end,
+    Port = open_port({spawn, Exec}, [{packet, 2}, binary, exit_status]),
     port_command(Port, [3, BPM, NTicks]),
-    receive 
-	{_, {data, <<0>> }} -> ok
+    receive
+        {_, {data, <<0>>}} -> ok
     after 5000 ->
-	    throw({Fdlink, not_responding, Port})
+        throw({Fdlink, not_responding, Port})
     end,
     Port.
 
 -spec stop(port()) -> true.
-stop(Port) ->	   
+stop(Port) ->
     port_command(Port, [?HWCLOCK_STOP]).
 
 -spec get_tick(binary()) -> non_neg_integer().
-get_tick(Opaque) ->   
-    <<0,50,Tick:32>> = Opaque,
+get_tick(Opaque) ->
+    <<0, 50, Tick:32>> = Opaque,
     Tick.
 
--spec get_tick_ms(non_neg_integer(),binary()) -> non_neg_integer().
-get_tick_ms(BPM, Opaque) ->  
-    case  Opaque of
-	<<0,50,Tick:32>>  ->
-	    round((1000*BPM*Tick)/(60*128));
-	Other -> 
-	    io:format("get_tick_ms Unknow Value: ~p.~n", [Other]),  %% FIXME
-	    Other
+-spec get_tick_ms(non_neg_integer(), binary()) -> non_neg_integer().
+get_tick_ms(BPM, Opaque) ->
+    case Opaque of
+        <<0, 50, Tick:32>> ->
+            round((1000 * BPM * Tick) / (60 * 128));
+        Other ->
+            %% FIXME
+            io:format("get_tick_ms Unknow Value: ~p.~n", [Other]),
+            Other
     end.
-
--- src/hwclock_bench.erl
+++ src/hwclock_bench.erl
@@ -1,67 +1,81 @@
 -module(hwclock_bench).
 
 -ignore_xref([start/0]).
--export([ start/0
-	]).
+-export([start/0]).
 
 %%--------------------------------------------------------
 %% erl -noshell -pa _build/default/lib/hwclock/ebin -s hwclock_bench | tee output_eee.dat
 -define(REFRESH_SCREEN, timer:seconds(10)).
--define(GAME_OVER, 60*?REFRESH_SCREEN).
--define(E_TIMER, 100). %% ms.
+-define(GAME_OVER, 60 * ?REFRESH_SCREEN).
+%% ms.
+-define(E_TIMER, 100).
 
--record(state, {e_clock_cumul       ::integer(), 
-		hw_clock_cumul      ::integer(),
-		timer         ::port(), 
-		bpm           ::byte(),
-		start_us         ::term(),
-		hwclock_every_ms :: integer(),
-		iter      ::integer()
-	       }).
-
+-record(state, {
+    e_clock_cumul :: integer(),
+    hw_clock_cumul :: integer(),
+    timer :: port(),
+    bpm :: byte(),
+    start_us :: term(),
+    hwclock_every_ms :: integer(),
+    iter :: integer()
+}).
 
 -spec start() -> true.
-start()  ->
-    BPM = 60,   %% between 1..255, 60 means 60 beats per minute
-    NTicks= 128, %% between 1..255, nticks = 128 with bpm = 60 ensures we receive an hwclock event every 1 second
+start() ->
+    %% between 1..255, 60 means 60 beats per minute
+    BPM = 60,
+    %% between 1..255, nticks = 128 with bpm = 60 ensures we receive an hwclock event every 1 second
+    NTicks = 128,
     HWCLOCK_interval_ms = 1000 * 60 * NTicks div BPM div 128,
 
-    io:format("~nhwclock:open(_BPM=~p, _NTicks=~p)~nevents scheduled every - erlang:~p ms, hwclock:~p ms~nstats are reported every ~p ms.~n",
-	      [BPM, NTicks, ?E_TIMER, HWCLOCK_interval_ms, ?REFRESH_SCREEN]),
+    io:format(
+        "~nhwclock:open(_BPM=~p, _NTicks=~p)~nevents scheduled every - erlang:~p ms, hwclock:~p ms~nstats are reported every ~p ms.~n",
+        [BPM, NTicks, ?E_TIMER, HWCLOCK_interval_ms, ?REFRESH_SCREEN]
+    ),
     Port = hwclock:open(BPM, NTicks),
     Now = os:timestamp(),
     erlang:start_timer(?E_TIMER, self(), erlang_timer),
     erlang:start_timer(?REFRESH_SCREEN, self(), stat_collect),
     erlang:start_timer(?GAME_OVER, self(), game_over),
-    loop_bench(#state{start_us=Now, timer=Port, bpm=BPM, e_clock_cumul=0, hw_clock_cumul=0, hwclock_every_ms= HWCLOCK_interval_ms}).
+    loop_bench(#state{
+        start_us = Now,
+        timer = Port,
+        bpm = BPM,
+        e_clock_cumul = 0,
+        hw_clock_cumul = 0,
+        hwclock_every_ms = HWCLOCK_interval_ms
+    }).
 
-loop_bench(S)  ->
+loop_bench(S) ->
     receive
-	{_Port, {data, _Opaque}} -> 
-	    %% Tick = hwclock:get_tick(Opaque),
-	    %% ElapsedMs = hwclock:get_tick_ms(S#state.bpm, Opaque),
-	    %% io:format("hw new data, tick number: ~p, ticks_ms: ~p ms.~n", [Tick, ElapsedMs]),
-	    Cumul = S#state.hw_clock_cumul,
-	    loop_bench(S#state{hw_clock_cumul=Cumul+1});
-
-	{timeout, _, erlang_timer} ->
-	    erlang:start_timer(?E_TIMER, self(), erlang_timer),
-	    Cumul = S#state.e_clock_cumul,
-	    loop_bench(S#state{e_clock_cumul=Cumul+1});
-
-	{timeout, _, stat_collect} ->
-	    erlang:start_timer(?REFRESH_SCREEN, self(), stat_collect),
-	    Elapsed_us =  timer:now_diff(os:timestamp(),S#state.start_us),
-	    io:format("stats after ~p us.\taccumulated events erlang:~p, hwclock:~p\tdrift us. erl versus hwclock: ~p~n",[Elapsed_us,
-														    S#state.e_clock_cumul, S#state.hw_clock_cumul,
-														    S#state.e_clock_cumul*?E_TIMER - S#state.hw_clock_cumul*S#state.hwclock_every_ms]),
-	    loop_bench(S);
-
-	{timeout, _, game_over} ->
-	    io:format("elapsed ! ~n"),
-	    hwclock:stop(S#state.timer);
-
-	Tellme ->
-	    io:format("abort on event ~w  ~n", [Tellme]),
-	    hwclock:stop(S#state.timer)
+        {_Port, {data, _Opaque}} ->
+            %% Tick = hwclock:get_tick(Opaque),
+            %% ElapsedMs = hwclock:get_tick_ms(S#state.bpm, Opaque),
+            %% io:format("hw new data, tick number: ~p, ticks_ms: ~p ms.~n", [Tick, ElapsedMs]),
+            Cumul = S#state.hw_clock_cumul,
+            loop_bench(S#state{hw_clock_cumul = Cumul + 1});
+        {timeout, _, erlang_timer} ->
+            erlang:start_timer(?E_TIMER, self(), erlang_timer),
+            Cumul = S#state.e_clock_cumul,
+            loop_bench(S#state{e_clock_cumul = Cumul + 1});
+        {timeout, _, stat_collect} ->
+            erlang:start_timer(?REFRESH_SCREEN, self(), stat_collect),
+            Elapsed_us = timer:now_diff(os:timestamp(), S#state.start_us),
+            io:format(
+                "stats after ~p us.\taccumulated events erlang:~p, hwclock:~p\tdrift us. erl versus hwclock: ~p~n",
+                [
+                    Elapsed_us,
+                    S#state.e_clock_cumul,
+                    S#state.hw_clock_cumul,
+                    S#state.e_clock_cumul * ?E_TIMER -
+                        S#state.hw_clock_cumul * S#state.hwclock_every_ms
+                ]
+            ),
+            loop_bench(S);
+        {timeout, _, game_over} ->
+            io:format("elapsed ! ~n"),
+            hwclock:stop(S#state.timer);
+        Tellme ->
+            io:format("abort on event ~w  ~n", [Tellme]),
+            hwclock:stop(S#state.timer)
     end.
