c278e243bde178912a5f4e8b7bc748bf8c965ec1
bougueil <rmariana@gmail.com>
2025-10-04 18:03:08 +0200

    change get_tick api, implement ALSA PPQ

--- src/hwclock.erl
+++ src/hwclock.erl
@@ -15,9 +15,9 @@
 -module(hwclock).
 
 -export([
-    open/2,
+    open/2, open/3,
     get_tick/1,
-    get_tick_ms/2,
+    get_tick_s/3,
     stop/1
 ]).
 
@@ -27,17 +27,62 @@
 -compile([export_all]).
 -endif.
 
-%% 15625 us. => RESOLUTION 2
--define(HWRES, 15625).
 -define(HWCLOCK_START, 3).
 -define(HWCLOCK_STOP, 4).
 
+-doc """
+Start an `hwclock` device with BPM and NTicks, where :
+ - BPM is an integer between 1 and 255 defining the tempo
+ - Nticks is an integer between 1 and 255 defining the number of ticks in a tempo
+
+Equivalent to open(BPM, NTick, 128).
+""".
 -spec open(byte(), byte()) -> port().
 open(BPM, NTicks) when
-    NTicks >= 0,
+    NTicks > 0,
     NTicks < 256,
-    BPM >= 0,
+    BPM > 0,
     BPM < 256
+->
+    open(BPM, NTicks, 128).
+
+% open hwclock with BPM, NTicks and PPQ, where :
+-doc """
+Start an `hwclock` device with BPM and NTicks, where :
+ - BPM is an integer between 1 and 255 defining the tempo
+ - Nticks is an integer between 1 and 255 defining the number of ticks in a tempo
+ - PPQ is an between 1 and 1024, see duration_ms calculation
+
+```bash
+# duration between hwclock events
+duration_ms = 1000* 60/bpm * nticks/128 * 128/ppq 
+```
+Returns the port of the `hwclock`
+## Examples
+
+```erlang
+%% receive an event every 1000 ms.
+1> hwclock:open(60, 128, 128).
+#Port<0.21>
+
+%% receive an event every 500 ms.
+1> hwclock:open(60, 64, 128).
+
+%% receive an event every 500 ms.
+1> hwclock:open(120, 128, 128).
+
+%% receive an event every 250 ms.
+1> hwclock:open(120, 128, 256).
+```
+""".
+-spec open(byte(), byte(), integer()) -> port().
+open(BPM, NTicks, PPQ) when
+    NTicks > 0,
+    NTicks < 256,
+    BPM > 0,
+    BPM < 256,
+    PPQ > 0,
+    PPQ =< 1024
 ->
     Fdlink = "fdlink_" ++ ?MODULE_STRING,
     Exec =
@@ -52,10 +97,13 @@ open(BPM, NTicks) when
             Dir ->
                 filename:join(Dir, Fdlink)
         end,
-    Port = open_port({spawn, Exec}, [{packet, 2}, binary, exit_status]),
-    port_command(Port, [3, BPM, NTicks]),
+    Port = open_port({spawn_executable, Exec}, [
+        {packet, 2}, binary, exit_status, {args, [integer_to_list(PPQ)]}
+    ]),
+    port_command(Port, [?HWCLOCK_START, 2, BPM, NTicks]),
     receive
-        {_, {data, <<0>>}} -> ok
+        {Port, {data, <<0>>}} ->
+            ok
     after 5000 ->
         throw({Fdlink, not_responding, Port})
     end,
@@ -70,13 +118,13 @@ get_tick(Opaque) ->
     <<0, 50, Tick:32>> = Opaque,
     Tick.
 
--spec get_tick_ms(non_neg_integer(), binary()) -> non_neg_integer().
-get_tick_ms(BPM, Opaque) ->
+-spec get_tick_s(binary(), non_neg_integer(), non_neg_integer()) ->
+    {integer(), float()} | nil.
+get_tick_s(Opaque, BPM, PPQ) ->
     case Opaque of
         <<0, 50, Tick:32>> ->
-            round((1000 * BPM * Tick) / (60 * 128));
+            {Tick, 60 * Tick / BPM / PPQ};
         Other ->
-            %% FIXME
-            io:format("get_tick_ms Unknow Value: ~p.~n", [Other]),
-            Other
+            io:format("Error, invalid tick format: ~w.~n", [Other]),
+            nil
     end.
